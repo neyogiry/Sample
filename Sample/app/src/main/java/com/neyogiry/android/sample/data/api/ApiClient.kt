14f44fbe56ac848026fbee967af142974b835ac5
neyogiry <yrigoyen.fernando@gmail.com>
2021-02-17 18:53:28 -0500

    feat: Added api client

---
+++ Sample/app/src/main/java/com/neyogiry/android/sample/data/api/ApiClient.kt
@@ -0,0 +1,37 @@
+package com.neyogiry.android.sample.data.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+
+    private const val TAG = "ApiClient"
+
+    private const val PATH = ""
+
+    private const val READ_TIMEOUT: Long = 60
+    private const val CONNECT_TIMEOUT: Long = 60
+
+    fun getApiService(): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(PATH)
+            .client(getBasicClientInterceptor())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    private fun getBasicClientInterceptor(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val builder = OkHttpClient.Builder()
+        builder.interceptors().add(logging)
+        builder.readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+        return builder.build()
+    }
+
+}
---
+++ Sample/app/src/main/java/com/neyogiry/android/sample/data/api/ApiService.kt
@@ -0,0 +1,7 @@
+package com.neyogiry.android.sample.data.api
+
+/**
+ * REST API access points
+ */
+interface ApiService {
+}
